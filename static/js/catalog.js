50c66fdc4197bd811fad93afb5fdb66fa111f02c
DesignDeveloperr <693.08wot@gmail.com>
2020-10-18 02:48:07 +0600

    add catalog

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -8,19 +8,21 @@ let get_json_catalog = (page) => {
     $.get('/catalog/json/' + page + '/' + search_query + '/' + order_by + '/', (data) => {
         for (i in data) {
             let item = data[i]
-            $('#products').append(
-                '<div class="xl:col-span-4 lg:col-span-6 col-span-12 rounded bg-white p-4 product_item">\n' +
-                '    <img src="' + item.image + '" class="object-cover h-64 w-full rounded" alt="">\n' +
-                '    <div class="font-medium my-2 text-2xl">' + item.name + '</div>\n' +
-                '    <div class="mb-2">' + item.price + '</div>\n' +
-                '    <div>' + item.date + '</div>\n' +
-                '</div>'
-            )
+            setTimeout(function () {
+                $('#products').append(
+                    '<div class="xl:col-span-4 lg:col-span-6 col-span-12 rounded bg-white p-4 product_item animate__animated animate__fadeIn">\n' +
+                    '    <img src="' + item.image + '" class="object-cover h-64 w-full rounded" alt="">\n' +
+                    '    <div class="font-medium my-2 text-2xl">' + item.name + '</div>\n' +
+                    '    <div class="mb-2">' + item.price + '</div>\n' +
+                    '    <div>' + item.date + '</div>\n' +
+                    '</div>'
+                )
+            }, 200 * i)
         }
     })
 }
 
 get_json_catalog(1)
 
-$('input[name="search"]').keyup(() => get_json_catalog(1))
+$('input[name="search"]').change(() => get_json_catalog(1))
 $('input[name="filter"]').click(() => get_json_catalog(1))
