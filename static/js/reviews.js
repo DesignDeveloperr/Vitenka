6642a10c9217375abfc570b487e3c98ed98fb659
DesignDeveloperr <693.08wot@gmail.com>
2020-10-17 21:15:37 +0600

    add reviews

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -4,7 +4,7 @@ $('#send_review').click(() => {
     //Выполняется при успешном ответе от сервера
     let review_success = (data) => { $('#review_form').hide(); $('#review_success').html(data.message); show_review_from_json() }
 
-    //Посылаем AJAX запрос на сервер
+    //Посылаем AJAX (AJAJ - Asynchronous Javascript And JSON) запрос на сервер
     $.ajax({
         url: location.href,
         //Получаем данные с полей формы
@@ -20,20 +20,30 @@ $('#send_review').click(() => {
 //Показываем отзывы на странице
 let show_review_from_json = () => {
 
+    //Удаляем отзывы со страницы
     $('.review_item').remove()
 
+    //Получаем JSON массив с отзывавми и показываем их
     $.get(json_reviews_url, (data) => {
-        for (let i in data) {
-            setTimeout(function () {
-                let item = data[i]
-                $('#reviews').append(
-                    '<div class="container mx-auto bg-white p-8 rounded mb-4 review_item animate__animated animate__fadeIn">\n' +
-                    '    <div class="text-2xl">' + item.name + '</div>\n' +
-                    '    <div class="text-gray-700 my-2">' + item.text + '</div>\n' +
-                    '    <div class="text-gray-500">' + item.date + '</div>\n' +
-                    '</div>'
-                )
-            }, 200 * i)
+        if (data.length > 0) {
+            for (let i in data) {
+                setTimeout(function () {
+                    let item = data[i]
+                    $('#reviews').append(
+                        '<div class="container mx-auto bg-white p-8 rounded mb-4 review_item animate__animated animate__fadeIn">\n' +
+                        '    <div class="text-2xl">' + item.name + '</div>\n' +
+                        '    <div class="text-gray-700 my-2">' + item.text + '</div>\n' +
+                        '    <div class="text-gray-500">' + item.date + '</div>\n' +
+                        '</div>'
+                    )
+                }, 200 * i)
+            }
+        } else {
+            $('#reviews').append(
+                '<div class="container mx-auto bg-white p-8 rounded mb-4 review_item animate__animated animate__fadeIn">\n' +
+                '    <div class="text-2xl text-center">Отзывов пока нет</div>\n' +
+                '</div>'
+            )
         }
     })
 }
